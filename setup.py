398a9f8da601b0cf99f2c0dab914756e91415e24
Zach Kurtz <zkurtz@protonmail.com>
2022-11-29 10:18:44 -0500

    initialize imbalance notebook

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='src',
+    version='0.0.0',
+    author="Zach Kurtz",
+    author_email="zkurtz@gmail.com",
+    description="sandbox",
+    license='MIT',
+)
